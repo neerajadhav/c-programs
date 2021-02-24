48e6a8b1d0c2fb20df2155e826fa0d460ef0fb0e
neerajadhav <adhavneeraj9500@gmail.com>
2021-02-24 16:13:52 +0000

    start of fifth program

---
+++ use-of-logical-operator.c
@@ -0,0 +1,20 @@
+/* use of logical operators */
+/* Author : Neeraj Adhav */
+/* Date : 24/2/2021 */
+
+/* The marks obtained by a student in 5 different subjects are
+ input through the keyboard. The student gets a division
+ as per the following rules:
+ Percentage above or equalt to 60 - First division
+ Percentage between 50 and 59 - Second division
+ Percentage between 40 and 49 - Third division
+ Percentage less than 40 - Fail */
+
+ #include <stdio.h>
+
+ int main(){
+    
+    
+
+    return 0;
+ }
